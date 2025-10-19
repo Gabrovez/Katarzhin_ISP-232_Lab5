ecabade6264678daca00e85404a2c4eca0c8a32d
Gabrovez <gabrovez321@gmail.com>
2025-10-19 19:16:06 +0300

    Шаг 4. Методы внутри класса (функции как часть объекта)

--- src/Hero.kt
+++ src/Hero.kt
@@ -6,4 +6,41 @@ class Hero {
     var mp: Int = 50
     var level: Int = 1
     var element: String = "Без стихии"
+    fun sayHello(){
+        println("Я - $name, мой путь только начинается!")
+    }
+    fun showStats(){
+        println("Имя: $name | Класс: $role | Здоровье: $hp | Мана: $mp | Уровень: $level | Стихия: $element")
+    }
+    fun meditate() {
+        println("$name медитирует...")
+        mp += 20
+        println("Мана восстановлена! Текущая мана: $mp")
+    }
+    fun takeDamage() {
+        println("$name получает урон!")
+        hp -= 10
+        println("Осталось здоровья: $hp")
+    }
+    fun castSpell(){
+        if (mp != 0 ){
+            mp -= 10
+            println("$name Кастует фаерболл, потрачено 10 маны")
+        }else{
+            println("Недостаточно маны! У вас только 0.")
+        }
+    }
+    fun heal(){
+        if (mp != 0 ){
+            hp += 10
+            mp -= 10
+            println("Лечусь! Восстановлено здоровье, потрачено 10 маны.")
+            println("Здоровье: $hp | Мана: $mp")
+        }else{
+            println("Недостаточно маны! У вас только 0.")
+        }
+    }
+
+
+
 }
--- src/Person.kt
+++ src/Person.kt
@@ -1,25 +1,4 @@
 fun main() {
-    val naruto: Hero = Hero()
-    naruto.name = "Наруто Узумаки"
-    naruto.gender = "Мужской"
-    naruto.role = "Хокаге"
-    naruto.hp = 200
-    naruto.mp = 150
-    println("Имя: ${naruto.name} \nПол: ${naruto.gender} \nКласс: ${naruto.role}\nЗдоровье: ${naruto.hp}\nМана: ${naruto.mp}")
-    val sasuke = Hero()
-    sasuke.name = "Саске Учиха"
-    sasuke.gender = "Мужской"
-    sasuke.role = "Шиноби-отступник"
-    sasuke.hp = 120
-    sasuke.mp = 180
-    println("Имя: ${sasuke.name} \nПол: ${sasuke.gender} \nКласс: ${sasuke.role}\nЗдоровье: ${sasuke.hp}\nМана: ${sasuke.mp}")
-    val gojo = Hero()
-    gojo.name = "Годжо Сатору"
-    gojo.gender = "Мужской"
-    gojo.role = "Маг проклятий"
-    gojo.hp = 160
-    gojo.mp = 300
-    println("Имя: ${gojo.name} \nПол: ${gojo.gender} \nКласс: ${gojo.role}\nЗдоровье: ${gojo.hp}\nМана: ${gojo.mp}")
 
     val hero = Hero()
     println("Введите имя героя")
@@ -37,5 +16,23 @@ fun main() {
     println("Введите стихию героя")
     hero.element = readln()
     println("Имя: ${hero.name} \nПол: ${hero.gender} \nКласс: ${hero.role}\nЗдоровье: ${hero.hp}\nМана: ${hero.mp}\nУровень: ${hero.level}\nСтихия: ${hero.element}")
+    hero.sayHello()
+    hero.showStats()
+    hero.meditate()
+    hero.takeDamage()
+    hero.castSpell()
+    hero.castSpell()
+    hero.castSpell()
+    hero.castSpell()
+    hero.castSpell()
+    hero.castSpell()
+    hero.castSpell()
+    hero.castSpell()
+    hero.castSpell()
+    hero.castSpell()
+    hero.meditate()
+    hero.heal()
+    hero.heal()
+    hero.heal()
 
 }
