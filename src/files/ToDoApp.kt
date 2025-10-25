10fabdd70ef113fad339cbd4085a832297a09aa9
Gabrovez <gabrovez321@gmail.com>
2025-10-26 02:36:43 +0300

    Шаг 5: Создание простого текстового менеджера задач (To-Do) с сохранением в файл

--- src/files/ToDoApp.kt
+++ src/files/ToDoApp.kt
@@ -6,16 +6,57 @@ fun main(){
     val file = File("toDo.txt")
     println("Welcome to To-Do App!")
     while (true){
-        print("Enter a task (or 0 to exit): ")
-        val userInput = readln()
-        if (userInput == "0"){
-            break
-        }else{
-            file.appendText("UserInput\n")
-            println("Task saved!")
+        println("\n=== Task Menu ===")
+        println("1 - Add Task")
+        println("2 - Show all tasks")
+        println("3 - Delete tasks")
+        println("0 - Exit")
+        print("Select an action: ")
+        when (readln()){
+            "1" -> {
+                print("Enter a new task: ")
+                val task =readln()
+                file.appendText("$task\n")
+                println("Task added")
+            }
+            "2" -> {
+                println("All tasks:")
+                if (file.exists() && file.readLines().isNotEmpty()){
+                    file.readLines().forEachIndexed { index, task -> println("${index + 1}. $task")  }
+                }else{
+                    println("! Task list is empty.")
+                }
+            }
+            "3" -> {
+                val tasks = file.readLines().toMutableList()
+                if (tasks.isEmpty()){
+                    println("Task list is empty.")
+                }else{
+                    println("Tasks for delete:")
+                    tasks.forEachIndexed { index, task -> println("${index + 1}. $task")   }
+                }
+                print("Enter the task number to delete: ")
+                val num =readln().toIntOrNull()
+                if (num == null || num !in 1..tasks.size){
+                    println("Incorrect number")
+                }else{
+                    val removed = tasks.removeAt(num - 1)
+                    file.writeText("")
+                    tasks.forEach { file.appendText("$it\n") }
+                    println("Task \"$removed\" deleted.")
+                }
+            }
+            "0" -> {
+                println("Exit from the app.")
+                break
+            }
+            else -> println("Incorrect input. Try again.")
         }
+
+
+
+
     }
 
-    println("\n Your To-DO list:")
-    println(file.readText())
+
 }
