aa1ce2b7275e6bbce4110f0a7d71005dd04bf70e
Gabrovez <gabrovez321@gmail.com>
2025-10-26 01:39:19 +0300

    Шаг 1: Создание перечисления QuestType

---
+++ src/world/QuestType.kt
@@ -0,0 +1,11 @@
+package world
+
+enum class QuestType(val description: String) {
+    DELIVERY("Deliver of item"),
+    ELIMINATION("Elimination of enemy"),
+    ESCORT("Escort of person"),
+    EXPLORE("Exploration of new territory"),
+    BOSSFIGHT("Fight with boss")
+
+
+}
