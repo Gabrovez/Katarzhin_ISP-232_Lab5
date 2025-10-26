292bd0a45ecbfcd131172a2d9fd76aefe16e012b
Gabrovez <gabrovez321@gmail.com>
2025-10-26 03:56:10 +0300

    Задание 1: Работа с квестами

--- src/world/Guild.kt
+++ src/world/Guild.kt
@@ -11,14 +11,33 @@ fun main(){
         println("High reward? ${if (miss.isHighReward()) "Yes" else "No"}")
         println()
     }*/
-    val escortQuest = Quest(
-        title =  "Escort trader to the village",
-        duration = 4,
-        reward = 120,
-        difficulty = "Medium",
-        questType = QuestType.ESCORT
+    val quests = listOf<Quest>(
+        Quest(title = "Escort trader to the village", duration = 4, reward = 120, difficulty = "Medium",questType = QuestType.ESCORT),
+        Quest(title = "Eliminate group of hostile orcs",duration = 2, reward = 250, difficulty = "Medium",questType = QuestType.ELIMINATION),
+        Quest("Explore the forgotten lands",  20, 500, "Medium", QuestType.EXPLORE),
+        Quest("Delivery pizza to the trade guild", 1, 100, "Easy", QuestType.DELIVERY),
+        Quest("Defeat the ancient evil", 12, 2500, "Hard", QuestType.BOSSFIGHT),
+        Quest("Explore the forbidden realms",  100, 2000, "Hard", QuestType.EXPLORE),
     )
-    escortQuest.printInfo()
+    for (ques in quests){
+        ques.describe()
+        ques.printInfo()
+        println()
+    }
+    println("Only Explore")
+    fun onlyExplore(){
+        for (ques in quests){
+            if(ques.questType == QuestType.EXPLORE){
+                ques.describe()
+                ques.printInfo()
+                println()
+            }
+        }
+    }
+    onlyExplore()
+
+
+
 
 
 
